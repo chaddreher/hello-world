37446c98b408019b42ec686c3bf9ddb715918ad0
Chad Dreher <chad.dreher@pcc.edu>
2016-10-25 13:56:19 -0700

    Add main method to say hello world

--- src/chad/dreher/hello/world/helloworld.java
+++ src/chad/dreher/hello/world/helloworld.java
@@ -1,7 +1,12 @@
 package chad.dreher.hello.world;
 
+
 /**
  * Created by Chad on 10/25/2016.
  */
 public class helloworld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
 }
